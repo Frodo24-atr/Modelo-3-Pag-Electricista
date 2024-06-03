c6fb99c91424060f9be8a552ac2e88f1d55ebb30
Frodo7u7-24-atr <emafran2015@gmail.com>
2024-06-03 17:49:22 -0300

    se corrigieron comportamientos del menu desplegable

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 /*botones navbar*/   /*botones navbar*/  /*botones navbar*/  /*botones navbar*/  /*botones navbar*/
-
 document.addEventListener('DOMContentLoaded', function () {
-    const menuCheckbox = document.getElementById('toggle-menu');
-    const menuItems = document.querySelectorAll('.navbar-list li a');
-  
-    menuItems.forEach(item => {
-      item.addEventListener('click', function (event) {
-        // Obtener el href del enlace
-        const targetId = this.getAttribute('href');
-  
-        if (targetId === '#') {
-          // Si el href es '#', evitar el comportamiento por defecto y desplazarse al inicio
-          event.preventDefault();
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-          });
-        } else if (targetId.startsWith('#')) {
-          // Prevenir el comportamiento por defecto del enlace
-          event.preventDefault();
-          
-          // Desplazarse a la sección correspondiente
-          document.querySelector(targetId).scrollIntoView({
-            behavior: 'smooth'
-          });
-        }
-  
-        // Cerrar el menú desmarcando el checkbox
-        menuCheckbox.checked = false;
-      });
+  const menuCheckbox = document.getElementById('toggle-menu');
+  const menuItems = document.querySelectorAll('.navbar-list li a');
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', function (event) {
+      // Obtener el href del enlace
+      const targetId = this.getAttribute('href');
+
+      if (targetId === '#') {
+        // Si el href es '#', evitar el comportamiento por defecto y desplazarse al inicio
+        event.preventDefault();
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      } else if (targetId.startsWith('#')) {
+        // Prevenir el comportamiento por defecto del enlace
+        event.preventDefault();
+        
+        // Desplazarse a la sección correspondiente
+        document.querySelector(targetId).scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+
+      // Cerrar el menú desmarcando el checkbox
+      menuCheckbox.checked = false;
     });
   });
+
+  // Cerrar el menú desplegable al hacer scroll
+  window.addEventListener('scroll', () => {
+      if (menuCheckbox.checked) {
+          menuCheckbox.checked = false;
+      }
+  });
+});
+
+  
 
   /*botones seccion presentacion*/  /*botones seccion presentacion*/  /*botones seccion presentacion*/
 
